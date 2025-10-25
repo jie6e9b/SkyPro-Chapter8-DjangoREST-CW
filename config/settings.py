ce2b61f19e43e765f633412c8af336d7643c1af8
jie6e9b <lvv1985@gmail.com>
2025-10-25 18:34:49 +0700

    merge conflicts

--- config/settings.py
+++ config/settings.py
@@ -158,6 +158,8 @@ CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
 CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/1")
 CELERY_TIMEZONE = TIME_ZONE
 
+# ------------------------ Настройка ауентификаации  ------------------------
+AUTH_USER_MODEL = "accounts.User"
 
 # ------------------------ Telegram ------------------------
 # Токен Telegram-бота
